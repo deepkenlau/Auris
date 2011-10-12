22449ec8061e6f1e8758046e5a67966c016379d9
Fabian Schuiki <fabianschuiki@axamblis.com>
2011-10-12 16:46:23 +0200

    Initial commit with basic channel class design.

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main(int argc, char ** argv)
+{
+    std::cout << "In Sono Veritas." << std::endl;
+    
+    //Initialize the listening HTTP listening socket.
+    
+    return 0;
+}
