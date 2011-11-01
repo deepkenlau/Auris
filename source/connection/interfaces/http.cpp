cd49eb9e776bee2ee0c06b903811afe8eab99f0b
Sandro Sgier <ssgier@tardis-c23.ee.ethz.ch>
2011-11-01 20:51:52 +0100

    updated onResponseData

--- source/connection/interfaces/http.cpp
+++ source/connection/interfaces/http.cpp
@@ -39,10 +39,12 @@ void ConnectionInterfaceHTTP::Handler::onResponseData(Response * r, const char *
 	if(!headerSent)
 	{
 		std::stringstream header;
-		header << "HTTP/1.1 200 OK\r\n";
-		header << "Content-Length: " << length << "\r\n";
+		header << "HTTP/1.1 "
+		if(r->isSuccessful()) header << "200 OK\r\n"; else header << "500 internal server error\r\n";
+		if(length) header << "Content-Length: " << length << "\r\n";
 		header << "\r\n";
 		write(fd, header.str().c_str(), header.str().size());
+		headerSent = true;
 	}
 
 	write(fd, data, length);
