db21cc06a1dd4f8763cc769bf087d7de5f0ee02e
ssgier <ssgier@student.ethz.ch>
2012-10-09 14:14:28 +0100

    completed Socket.h

--- common/Socket.h
+++ common/Socket.h
@@ -3,9 +3,12 @@
 
 class Socket
 {
+	
 public:
+	static Socket* makeListening(int port);
+	static Socket* makeConnected(std::string hostname, int port);
+	Socket* accept();
 	bool poll(int timeout);
-
 	int read(char *buffer, unsigned int length);
 	int write(const char *source, unsigned int length);
 };
