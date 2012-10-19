f2ae0c1cc384becd7c5b5f2e286d7ee88a220183
ssgier <ssgier@student.ethz.ch>
2012-10-19 11:51:22 +0100

    implemented getFormats

--- tests/Mediastore.cpp
+++ tests/Mediastore.cpp
@@ -1,18 +1,29 @@
 #include <database/media/Store.h>
 #include <iostream>
+#include <set>
 
 using namespace std;
 using database::media::Store;
 
 int main(void)
-{
-	Path path;
-	Store store(path);
-	/*char * str = "asdfg\n";
+{	
+	Store store("debug_media");
+	char * str = "asdfg\n";
 	Blob blob(str, 7);
-	store.persist(blob, "bubber.txt", "mp3 bitches!\n");
+	store.persist(blob, "bubber.txt", "mp3 bitches!");
+	store.persist(blob, "haha.txt", "wave, u jelly?");	
 
-	cout << (char*)store.load("bubber.txt", "mp3 bitches!\n").buffer << endl;*/
-	cout << store.getMainFormat("bubber.txt") << endl;
+	store.persist(blob, "gusta.txt", "wave, u jelly?");
+
+	cout << (char*)store.load("bubber.txt", "mp3 bitches!").buffer << endl;
+
+cout << "arrived here" << endl;
+
+
+	set<string> formats = store.getFormats();
+	for(set<string>::iterator it = formats.begin(); it != formats.end(); it++)
+	{
+		cout << *it << endl;
+	}
 	return 0;
 }
