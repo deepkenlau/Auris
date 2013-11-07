957219423a303c066dcae2446d169b39325cb339
Fabian Schuiki <fabian@schuiki.ch>
2013-11-07 07:56:30 +0100

    Some restructuring of the code.

---
+++ source/db/Database.cpp
@@ -0,0 +1,13 @@
+/* Copyright © 2013 Fabian Schuiki */
+#include "Database.hpp"
+
+using auris::db::Database;
+
+
+Database::Database(const Path& p): path(p)
+{
+}
+
+Database::~Database()
+{
+}
---
+++ source/db/Database.hpp
@@ -0,0 +1,22 @@
+/* Copyright © 2013 Fabian Schuiki */
+#pragma once
+#include <boost/filesystem/path.hpp>
+
+namespace auris {
+namespace db {
+
+class Database
+{
+public:
+	typedef boost::filesystem::path Path;
+	const Path path;
+
+	Database(const Path& p);
+	~Database();
+
+	void add();
+	void remove();
+};
+
+} // namespace db
+} // namespace auris
