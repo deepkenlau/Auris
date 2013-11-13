9345f5125245f34f8f8f30be642d54f3d2f0b7d4
Fabian Schuiki <fabian@schuiki.ch>
2013-11-13 07:59:56 +0100

    Added sha1 tool that uses boost.

---
+++ source/common/sha1.hpp
@@ -0,0 +1,84 @@
+/* Copyright © 2013 Fabian Schuiki */
+#pragma once
+#include <boost/uuid/sha1.hpp>
+#include <istream>
+#include <fstream>
+#include <string>
+
+namespace auris {
+
+class sha1
+{
+public:
+	sha1() {}
+	sha1(const sha1& s): sha(s.sha) {}
+	sha1(const char* buffer, size_t len) { from_buffer(buffer, len); }
+	sha1(const std::string& str) { from_string(str); }
+	sha1(std::istream& is) { from_stream(is); }
+
+	sha1& from_buffer(const char* buffer, size_t len)
+	{
+	    sha.process_bytes(buffer, len);
+		return *this;
+	}
+
+	sha1& from_string(const std::string& str)
+	{
+		sha.process_bytes(str.c_str(), str.length());
+		return *this;
+	}
+
+	sha1& from_stream(std::istream& is)
+	{
+		char buffer[1024];
+		while (is.good()) {
+			is.read(buffer, sizeof(buffer));
+			sha.process_bytes(buffer, is.gcount());
+		}
+		return *this;
+	}
+
+	sha1& from_file(const char* path)
+	{
+		std::ifstream f(path);
+		return from_stream(f);
+	}
+
+	size_t raw(char *dst)
+	{
+		char *dst_start = dst;
+		unsigned int hash[5];
+		sha.get_digest(hash);
+		for (int i = 0; i < 5; i++) {
+			for (int n = 24; n >= 0; n -= 8) {
+				*dst = (hash[i] >> n) & 0xFF;
+				dst++;
+			}
+		}
+		return dst - dst_start;
+	}
+
+	size_t hex(char *dst, bool trailing_zero = true)
+	{
+		char *dst_start = dst;
+		static const char *lookup = "0123456789abcdef";
+		unsigned int hash[5];
+		sha.get_digest(hash);
+		for (int i = 0; i < 5; i++) {
+			for (int n = 28; n >= 0; n -= 4) {
+				*dst = lookup[(hash[i] >> n) & 0xF];
+				dst++;
+			}
+		}
+		if (trailing_zero) {
+			*dst = 0;
+			dst++;
+		}
+		return dst - dst_start;
+	}
+
+private:
+	boost::uuids::detail::sha1 sha;
+};
+
+} // namespace auris
--- source/tools/db-add.cpp
+++ source/tools/db-add.cpp
@@ -1,8 +1,117 @@
 /* Copyright (c) 2013 Fabian Schuiki */
+#include <common/sha1.hpp>
 #include <iostream>
+#include <boost/program_options.hpp>
+#include <string>
+#include <vector>
+
+namespace po = boost::program_options;
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::vector;
+
+class DatabaseTool
+{
+public:
+	int argc;
+	char **argv;
+	po::variables_map vm;
+
+	string opt_repository;
+
+	DatabaseTool(int argc, char **argv): argc(argc), argv(argv)
+	{
+		opt_repository = ".";
+	}
+
+	void print_usage()
+	{
+		cout << "usage: " << *argv << ' ' << usage_string() << endl;
+	}
+
+	int run()
+	{
+		try {
+			// Configure the general command line options.
+			po::options_description general("General options");
+			general.add_options()
+				("help,h", "show help message")
+				("repository,r", po::value<string>(&opt_repository), "path of repository to operate on");
+			options.add(general);
+			visible_options.add(general);
+			build_options();
+
+			// Parse the command line options.
+			po::store(po::command_line_parser(argc, argv)
+				.options(options)
+				.positional(positional_options)
+				.run(), vm);
+			po::notify(vm);
+
+			// Show the help if required.
+			if (vm.count("help")) {
+				print_usage();
+				cout << visible_options << endl;
+				return 1;
+			}
+
+			// Call the subclass' main function.
+			return main();
+
+		} catch (std::exception &e) {
+			cerr << e.what() << endl;
+			return 1;
+		}
+		return 0;
+	}
+
+	virtual const char * usage_string() = 0;
+	virtual void build_options() {};
+	virtual int main() = 0;
+
+protected:
+	po::options_description options;
+	po::options_description visible_options;
+	po::positional_options_description positional_options;
+};
+
+class ThisTool : public DatabaseTool
+{
+public:
+	vector<string> opt_files;
+
+	ThisTool(int argc, char **argv): DatabaseTool(argc, argv) {}
+	const char* usage_string() { return "file1 ..."; }
+
+	void build_options()
+	{
+		po::options_description parameters("Parameters");
+		parameters.add_options()
+			("file", po::value< vector<string> >(&opt_files), "file to add");
+
+		options.add(parameters);
+		positional_options.add("file", -1);
+	}
+
+	int main()
+	{
+		cout << "running db-add on repo " << opt_repository << endl;
+		for (int i = 0; i < opt_files.size(); i++) {
+			char hex[41];
+			cout << "- adding " << opt_files[i];
+			auris::sha1().from_file(opt_files[i].c_str()).hex(hex);
+			cout << " " << hex << endl;
+		}
+
+		return 0;
+	}
+};
 
 int main(int argc, char *argv[])
 {
-	
-	return 0;
+	ThisTool t(argc, argv);
+	return t.run();
 }
