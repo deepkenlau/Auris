462b3adae8dadf9e832d0b6e2983e6f76dc7811e
Fabian Schuiki <fschuiki@student.ethz.ch>
2013-11-17 01:00:22 +0100

    db-meta: added -m -f -n options
    The db-meta command may now limit its output to metadata only (-m) or formats only (-f), and filter the displayed metadata fields (one or more -n <field>).

--- source/tools/db-meta.cpp
+++ source/tools/db-meta.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <map>
 #include <set>
+#include <vector>
 
 namespace auris {
 namespace tools {
@@ -14,6 +15,7 @@ namespace tools {
 using std::string;
 using std::map;
 using std::set;
+using std::vector;
 
 /**
  * @brief Tool that displays and modifies tracks.
@@ -23,12 +25,23 @@ class db_meta : public Generic
 public:
 	fs::path repo;
 	string opt_track;
+	bool opt_only_metadata;
+	bool opt_only_formats;
+	vector<string> opt_fields_vector;
+	set<string> opt_fields;
 
 	db_meta(int argc, char **argv): Generic(argc, argv) {}
 	const char* usage_string() const { return "<track>"; }
 
 	void build_options()
 	{
+		po::options_description parameters("Parameters");
+		parameters.add_options()
+			("only-metadata,m", "display only metadata")
+			("only-formats,f", "display only formats")
+			("field,n", po::value< vector<string> >(&opt_fields_vector), "filter metadata (multiple allowed)");
+		options.add(parameters);
+
 		hidden_options.add_options()
 			("track", po::value<string>(&opt_track), "track whose metadata is of interest");
 		positional_options.add("track", -1);
@@ -37,6 +50,9 @@ public:
 	int main()
 	{
 		repo = opt_repository;
+		opt_only_metadata = vm.count("only-metadata");
+		opt_only_formats = vm.count("only-formats");
+		opt_fields.insert(opt_fields_vector.begin(), opt_fields_vector.end());
 
 		if (opt_track.empty()) {
 			cerr << "no track specified\n";
@@ -51,16 +67,27 @@ public:
 		track.read(track_ifs);
 
 		cout << "# track " << nice_hash(track_ref) << " (" << track.formats.size() << " formats)\n";
-		cout << "Id: " << track.id << '\n';
-		for (map<string,string>::const_iterator it = track.md.begin(); it != track.md.end(); it++) {
-			cout << it->first << ": " << it->second << '\n';
+		if (!opt_only_formats) {
+			if (opt_fields.empty() || opt_fields.count("Id")) {
+				cout << "Id: " << track.id << '\n';
+			}
+			for (map<string,string>::const_iterator it = track.md.begin(); it != track.md.end(); it++) {
+				if (!opt_fields.empty() && !opt_fields.count(it->first))
+					continue; // if field filter is set, skip fields it does not name
+				cout << it->first << ": " << it->second << '\n';
+			}
 		}
-		cout << '\n';
-		for (set<auris::db::file::Track::Format>::const_iterator it = track.formats.begin(); it != track.formats.end(); it++) {
-			cout << nice_hash((*it).blob_ref);
-			if (!(*it).format.empty())
-				cout << "  <" << (*it).format << '>';
-			cout << "  " << (*it).orig_name << '\n';
+
+		if (!opt_only_metadata && !opt_only_formats)
+			cout << '\n';
+
+		if (!opt_only_metadata) {
+			for (set<auris::db::file::Track::Format>::const_iterator it = track.formats.begin(); it != track.formats.end(); it++) {
+				cout << nice_hash((*it).blob_ref);
+				if (!(*it).format.empty())
+					cout << "  <" << (*it).format << '>';
+				cout << "  " << (*it).orig_name << '\n';
+			}
 		}
 
 		return 0;
