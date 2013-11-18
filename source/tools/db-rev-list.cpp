fdf77dfc243a7bfcfa82c2e3065b10eb2fdf4157
Fabian Schuiki <fabian@schuiki.ch>
2013-11-18 09:27:45 +0100

    added db-rev-list tool
    The tool in db-rev-list.cpp lists all index revisions in the database. Basically
    it opens the index pointed to by refs/index, then using the "Base" field walks
    the entire history of revisions.

---
+++ source/tools/db-rev-list.cpp
@@ -0,0 +1,54 @@
+/* Copyright (c) 2013 Fabian Schuiki */
+#include "Generic.hpp"
+
+#include <aux/mapfile.hpp>
+#include <db/file/Index.hpp>
+#include <db/Object.hpp>
+
+#include <string>
+#include <set>
+
+namespace auris {
+namespace tools {
+
+using namespace aux;
+using std::string;
+using std::set;
+
+class db_rev_list : public Generic
+{
+public:
+	db_rev_list(int argc, char **argv): Generic(argc, argv) {}
+	const char* usage_string() const { return ""; }
+
+	int main()
+	{
+		// Read refs/index manually.
+		string initial_hash;
+		if (!mapfile::maybe_read(dbs.ref("index").path.c_str(), initial_hash)) {
+			cerr << "no index found\n";
+			return 1;
+		}
+
+		// Walk over the index revisions.
+		string hash = initial_hash;
+		while (!hash.empty())
+		{
+			db::ObjectBuffer<db::file::Index> index(dbs);
+			index.read(hash);
+			cout << hash << '\n';
+			hash = index.base;
+		}
+
+		return 0;
+	}
+};
+
+} // namespace tools
+} // namespace auris
+
+int main(int argc, char *argv[])
+{
+	auris::tools::db_rev_list t(argc, argv);
+	return t.run();
+}
