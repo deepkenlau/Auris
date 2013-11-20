0a0be1875fdf1b7dae27bacff0f6f9d94aee4084
Fabian Schuiki <fschuiki@student.ethz.ch>
2013-11-20 20:13:32 +0100

    db-list: added -v (verbose) option
    The db-list.cpp tool now prints its additional information ("# index ...") only
    if the --verbose/-v option is passed on the command line. The information is
    written to standard output now. In addition, the permanent track id shown is
    abbreviated by the standard nice_hash() call to make the list more readable.

--- source/tools/db-list.cpp
+++ source/tools/db-list.cpp
@@ -23,15 +23,27 @@ public:
 	db_list(int argc, char **argv): Generic(argc, argv) {}
 	const char* usage_string() const { return ""; }
 
+	void build_options()
+	{
+		po::options_description parameters("Parameters");
+		parameters.add_options()
+			("verbose,v", "show additional information");
+		options.add(parameters);
+	}
+
 	int main()
 	{
+		bool opt_verbose = vm.count("verbose");
+
 		db::ObjectBuffer<db::file::Index> index(dbs);
 		index.maybe_ref("index");
 
-		cerr << "# index " << nice_hash(index.hash_in) << ", " << index.tracks.size() << " tracks\n";
-		cerr << "# changed on " << index.date << '\n';
+		if (opt_verbose) {
+			cout << "# index " << nice_hash(index.hash_in) << ", " << index.tracks.size() << " tracks\n";
+			cout << "# changed on " << index.date << '\n';
+		}
 		for (map<string, string>::const_iterator it = index.tracks.begin(); it != index.tracks.end(); it++) {
-			cout << it->first << ' ' << nice_hash(it->second) << '\n';
+			cout << nice_hash(it->first) << ' ' << nice_hash(it->second) << '\n';
 		}
 		
 		return 0;
